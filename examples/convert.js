4aae24eee7747713f1ec2c2e5788938b4241d2c6
Makis Tracend <makis.tracend@gmail.com>
2013-04-25 15:51:21 -0700

    minor update in the console.log

--- examples/convert.js
+++ examples/convert.js
@@ -2,5 +2,5 @@ var threeOBJ = require("../index"),
 	fs = require("fs");
 
 threeOBJ.convert("./obj/palm.obj", "./ascii/palm.js", function( response ){
-	console.log( "File saved at: json/palm.js"  );
+	console.log( "File saved at: ascii/palm.js"  );
 });
