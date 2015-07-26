a8d3baeb8c26086dd43d522d05c3a8b0f7624087
Makis Tracend <makis.tracend@gmail.com>
2015-07-26 02:35:10 -0700

    - camaro example

--- examples/camaro.js
+++
@@ -1,8 +0,0 @@
-var threeOBJ = require("../index")(),
-	fs = require("fs");
-
-threeOBJ.set({ scale: 0.1 });
-
-threeOBJ.convert("./camaro/Camaro.obj", "./camaro/Camaro.js", function( response ){
-	console.log( "File saved at: camaro/Camaro.js"  );
-});
